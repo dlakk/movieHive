4cd8418fe9c81c00ba6eee92fac407edca7902d5
dlakk <otiialvin>
2024-12-09 00:59:36 +0300

    included a suggestion search bar

--- script.js
+++ script.js
@@ -1,14 +1,111 @@
+async function fetchSuggestions(query) {
+  const apiKey = "5e5b8093e7d7736405fb91d83905aaab";
+  try {
+    const [movieRes, tvShowsRes, peopleRes] = await Promise.all([
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
+      ),
+      fetch(
+        `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}`
+      ),
+      fetch(
+        `https://api.themoviedb.org/3/search/person?api_key=${apiKey}&query=${query}`
+      ),
+    ]);
+    const movieData = await movieRes.json();
+    const tvShowsData = await tvShowsRes.json();
+    const peopleData = await peopleRes.json();
+    // call the display suggestion
+    displaySuggestions(
+      movieData.results,
+      tvShowsData.results,
+      peopleData.results
+    );
+    console.log(movieData);
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+}
+function displaySuggestions(movies, tvShows, people) {
+  const suggestions = document.getElementById("suggestions");
+  suggestions.innerHTML = ""; // Clear previous suggestions
+
+  const allResults = [
+    { type: "Movie", results: movies },
+    { type: "TV Show", results: tvShows },
+    { type: "Crew", results: people },
+  ];
+
+  allResults.forEach((category) => {
+    if (category.results.length > 0) {
+      const categoryHTML = `
+                      
+                ${category.results
+                  .map(
+                    (item) => {
+                     const releaseYear = item.release_date ? item.release_date.split('-')[0] : '';
+                     return `
+                    <div class="suggestion-item" onclick="selectSuggestion('${
+                      item.title || item.name
+                    }')">
+                        <img class="suggestion-img" src="https://image.tmdb.org/t/p/w92${
+                          item.poster_path || item.profile_path || ""
+                        }" 
+                            alt="${item.title || item.name}">
+                            <div class="suggestion-des">
+                            <p class="suggestion-category">${category.type}</p>
+                            <span>${releaseYear}</span>
+                        <p class="suggestion-title">${item.title || item.name}</p>
+                        </div>
+                        <hr>
+                    </div>
+                `}
+                  )
+                  .join("")}
+          
+        `;
+      suggestions.innerHTML += categoryHTML;
+    }
+  });
+
+  suggestions.style.display = allResults.some(
+    (category) => category.results.length > 0
+  )
+    ? "block"
+    : "none";
+}
+function selectSuggestion(value) {
+  document.getElementById("search-input").value = value;
+  document.getElementById("suggestions").style.display = "none";
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
   const genreList = {
-    28: "Action",12: "Adventure",16: "Animation",35: "Comedy",
-    80: "Crime",99: "Documentary",18: "Drama",10751: "Family",
-    14: "Fantasy",36: "History",27: "Horror",10402: "Music",
-    9648: "Mystery",10749: "Romance",878: "Science Fiction",10770: "TV Movie",
-    53: "Thriller",10752: "War",37: "Western"
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Science Fiction",
+    10770: "TV Movie",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
   };
   const categories = [
-    { id: "now_playing", name: "Now Playing" },{ id: "popular", name: "Popular" },
-    { id: "top_rated", name: "Top Rated" },{ id: "upcoming", name: "Upcoming" },
+    { id: "now_playing", name: "Now Playing" },
+    { id: "popular", name: "Popular" },
+    { id: "top_rated", name: "Top Rated" },
+    { id: "upcoming", name: "Upcoming" },
   ];
   const authBtn = document.getElementById("auth");
 
@@ -28,14 +125,24 @@ document.addEventListener("DOMContentLoaded", async () => {
   let thumbnailDom = document.querySelector(".hero-section #thumbnail-slide");
 
   //fetch search
-
+  document.getElementById("search-input").addEventListener("input", (e) => {
+    fetchSuggestions(e.target.value);
+    //hide suggestion and close search bar
+    document
+      .querySelector(".search-container")
+      .addEventListener("mouseleave", () => {
+        document.getElementById("search-input").value = ""; //clear input field
+        const suggestions = document.getElementById("suggestions");
+        suggestions.style.display = "none"; //hide suggestions
+      });
+  });
   const fetchMovies = async () => {
     try {
       const res = await fetch(
         "https://api.themoviedb.org/3/trending/movie/week?api_key=5e5b8093e7d7736405fb91d83905aaab"
       );
       const data = await res.json();
-      
+
       return data.results;
     } catch (error) {
       console.error("Error fetching movies:", error);
@@ -55,7 +162,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   };
 
-  
   const fetchVideos = async (movieId) => {
     try {
       const res = await fetch(
@@ -124,7 +230,9 @@ document.addEventListener("DOMContentLoaded", async () => {
                 movie.poster_path
               }" alt="${movie.title}" data-id="${movie.id}">
               <div class="movie-info">
-              <p><i class="fa-solid fa-star"></i>${movie.vote_average.toFixed(1)}</p>
+              <p><i class="fa-solid fa-star"></i>${movie.vote_average.toFixed(
+                1
+              )}</p>
               <h3>${movie.title}</h3>
               </div>
             </div>
